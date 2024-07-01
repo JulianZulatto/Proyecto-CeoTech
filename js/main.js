d82c6ab1a370439125d0b0b0856957d446d4fb9b
julianzulatto <julianzulatto14@outlook.com>
2024-07-01 20:44:19 -0300

    funcionalidad js y videos

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+
+document.getElementById('buscador').addEventListener('input', function() {
+    let input = this.value.toLowerCase();
+    let videos = document.querySelectorAll('#listaVideos li');
+  
+    videos.forEach(function(video) {
+      let videoNombre = video.textContent.toLowerCase();
+      if (videoNombre.includes(input)) {
+        video.style.display = 'block';
+      } else {
+        video.style.display = 'none';
+      }
+    });
+  });
