7fe718a9bded2025c037bac3ce524879849ffcc0
julianzulatto <julianzulatto14@outlook.com>
2024-07-17 22:48:17 -0300

    proyecto funcional terminado

--- js/login.js
+++ js/login.js
@@ -1,60 +1,54 @@
 
 
 function registrarUsuario() {
-    let usuario = document.getElementById('usuario').value;
-    let contraseña = document.getElementById('contraseña').value;
+  let usuario = document.getElementById('usuario').value;
+  let contraseña = document.getElementById('contraseña').value;
 
-    crearUsuario(usuario, contraseña);
+  crearUsuario(usuario, contraseña);
 
-    Swal.fire("Se a registrado con exito!");
+  Swal.fire("Se a registrado con exito!");
 }
 
-
 function login() {
-    fetch('../js/data/usuarios.json')
-      .then(response => response.json())
-      .then(data => {
-        let usuario = document.getElementById('usuario_login').value;
-        let contraseña = document.getElementById('contraseña_login').value;
-  
-        let usuario_encontrado = data.find(u => u.usuario === usuario && u.contraseña === contraseña);
-  
-        if (usuario_encontrado) {
-          Swal.fire("Se ha logueado con éxito!");
-        } else {
-          Swal.fire("Usuario o contraseña equivocada, inténtelo nuevamente");
-        }
-      })
-      .catch(error => console.error('Error al cargar los usuarios:', error));
+  let usuario = document.getElementById('usuario_login').value;
+  let contraseña = document.getElementById('contraseña_login').value;
+  let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+  let usuario_encontrado = usuarios.find(u => u.usuario === usuario && u.contraseña === contraseña);
+
+  if (usuario_encontrado) {
+    localStorage.setItem("sesion-usuario", usuario_encontrado.id)
+    Swal.fire("Se ha logueado con éxito!");
+  } else {
+    Swal.fire("Usuario o contraseña equivocada, inténtelo nuevamente");
   }
+}
+
+function estoyLogueado() {
+  let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+  let id_usuario = parseInt(localStorage.getItem("sesion-usuario"));
+  let usuario = usuarios.find(u => u.id === id_usuario);
+
+  if (usuario) {
+      return true;
+  } else {
+      return false;
+  }
+}
 
 function crearUsuario(usuario, contraseña) {
-    fetch('../js/data/usuarios.json')
-      .then(response => response.json())
-      .then(data => {
-        let usuarios = data || [];
-        let id = Math.floor(Math.random() * 100);
-        let user = { usuario, contraseña, id };
-        usuarios.push(user);
-  
-        // Guardar los usuarios actualizados en el archivo usuarios.json
-        fetch('../js/data/usuarios.json', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(usuarios)
-        });
-      })
-      .catch(error => console.error('Error al cargar usuarios:', error));
-  }
+  let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+  let id = Math.floor(Math.random() * 100);
+  let user = { usuario, contraseña, id };
+  usuarios.push(user);
+  localStorage.setItem("usuarios", JSON.stringify(usuarios))
+}
 
 function cerrarSesion() {
-    localStorage.removeItem('usuarios');
+  localStorage.removeItem('sesion-usuario');
 }
 
 document.getElementById('cerrar_sesion')?.addEventListener('click', function () {
-    cerrarSesion();
+  cerrarSesion();
 });
 
 
--- js/tutoriales.js
+++ js/tutoriales.js
@@ -5,7 +5,7 @@ document.getElementById('buscador')?.addEventListener('input', function () {
     let input = this.value.toLowerCase();
     let videos = document.querySelectorAll('#listaVideos li');
 
-    videos.forEach(function (video) {
+    videos.forEach((video) => {
       let videoNombre = video.textContent.toLowerCase();
       if (videoNombre.includes(input)) {
         video.style.display = 'block';
@@ -17,6 +17,48 @@ document.getElementById('buscador')?.addEventListener('input', function () {
     alert('Necesitas estar logueado para poder utilizar esta funcion.')
   }
 });
+
+
+// Cargar Lista de Videos usando Fetch
+document.addEventListener('DOMContentLoaded', async () => {
+  let listaTutoriales = document.getElementById("listaVideos");
+  if (!listaTutoriales) return;
+
+  let tutoriales = await obtenerTutoriales();
+  tutoriales.forEach(tutorial => renderTutorial(tutorial, listaTutoriales))
+});
+
+async function obtenerTutoriales() {
+  try {
+    let response = await fetch("../js/data/tutoriales.json");
+    let json = await response.json();
+    return json;
+  } catch (error) {
+    console.error('Error al cargar tutoriales:', error);
+    return [];
+  }
+}
+
+
+function renderTutorial(data, list) {
+  let li = document.createElement("li");
+  let title = document.createElement("h5");
+  title.textContent = data.titulo;
+  li.appendChild(title);
+
+  let iframe = document.createElement("iframe");
+  iframe.width = "560";
+  iframe.height = "315";
+  iframe.src = data.link;
+  iframe.title = data.titulo;
+  iframe.frameBorder = "0";
+  iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
+  iframe.allowFullscreen = true;
+  iframe.referrerPolicy = "strict-origin-when-cross-origin";
+
+  li.appendChild(iframe);
+  list.appendChild(li);
+}
 
 
 
